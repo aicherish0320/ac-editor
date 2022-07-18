c5f5d1f5118f8853287417e3487649a2906750d4
aicherish0320 <aicherish0320@163.com>
2022-07-18 15:39:06 +0800

    home layout

---
+++ src/views/home/ContentCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content-card">
+    <a-row :gutter="16">
+      <a-col :span="6" v-for="item in 5" :key="item" class="card-item">
+        <a-card hoverable>
+          <template #cover>
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          </template>
+          <a-card-meta title="Europe Street beat">
+            <template #description>www.instagram.com</template>
+          </a-card-meta>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.content-card {
+  max-width: 1200px;
+  margin: 0 auto;
+  .card-item {
+    margin-bottom: 16px;
+  }
+}
+</style>
---
+++ src/views/home/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-layout>
+    <a-layout-header class="header"> 爱鹊絮 </a-layout-header>
+    <a-layout-content class="content">
+      <ContentCard></ContentCard>
+    </a-layout-content>
+    <a-layout-footer>footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<script setup lang="ts">
+import ContentCard from './ContentCard.vue'
+</script>
+
+<style scoped>
+.header {
+  color: #fff;
+  font-size: 20px;
+}
+.content {
+  background: #fff;
+}
+</style>
