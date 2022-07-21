21ae918ecca1a0779550994919ae773023f9e9f7
aicherish0320 <aicherish0320@163.com>
2022-07-21 17:34:36 +0800

    upload list

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="file-upload">
-    <a-button type="primary" @click="triggerUpload">
-      <span v-if="fileStatus === 'loading'">正在上传</span>
-      <span v-else-if="fileStatus === 'success'">上传成功</span>
-      <span v-else-if="fileStatus === 'error'">上传失败</span>
+    <a-button type="primary" @click="triggerUpload" :disabled="isUploading">
+      <span v-if="isUploading">正在上传</span>
       <span v-else>点击上传</span>
     </a-button>
     <input
@@ -12,13 +10,34 @@
       :style="{ display: 'none' }"
       @change="handleFileChange"
     />
+    <ul>
+      <li
+        :class="`uploaded-file upload-${uploadedFile.status}`"
+        v-for="uploadedFile in uploadedFiles"
+        :key="uploadedFile.uid"
+      >
+        <span> {{ uploadedFile.name }}</span>
+        <a-button type="danger" @click="removeFile(uploadedFile.uid)"
+          >Del</a-button
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
+import { v4 as uuidV4 } from 'uuid'
 
 type UploadStatus = 'ready' | 'loading' | 'success' | 'error'
+
+interface UploadFile {
+  uid: string
+  size: number
+  name: string
+  status: UploadStatus
+  raw: File
+}
 
 const props = defineProps<{
   action: string
@@ -31,6 +50,14 @@ const triggerUpload = () => {
     fileInput.value.click()
   }
 }
+const uploadedFiles = ref<UploadFile[]>([])
+const isUploading = computed(() =>
+  uploadedFiles.value.some((file) => file.status === 'loading')
+)
+const removeFile = (id: string) => {
+  console.log('id >>> ', id)
+  uploadedFiles.value = uploadedFiles.value.filter((file) => file.uid !== id)
+}
 
 const handleFileChange = (e: Event) => {
   const target = e.target as HTMLInputElement
@@ -40,18 +67,32 @@ const handleFileChange = (e: Event) => {
     const uploadedFile = files[0]
     const formData = new FormData()
     formData.append('file', uploadedFile)
+    const fileObj = reactive<UploadFile>({
+      uid: uuidV4(),
+      size: uploadedFile.size,
+      name: uploadedFile.name,
+      status: 'loading',
+      raw: uploadedFile
+    })
+    uploadedFiles.value.push(fileObj)
 
     fetch(props.action, {
       method: 'post',
       body: formData
     })
       .then((response) => response.json())
-      .then((ret) => {
+      .then(() => {
         fileStatus.value = 'success'
-        console.log('ret >>> ', ret)
+        fileObj.status = 'success'
       })
       .catch(() => {
         fileStatus.value = 'error'
+        fileObj.status = 'error'
+      })
+      .finally(() => {
+        if (fileInput.value) {
+          fileInput.value.value = ''
+        }
       })
   }
 }
